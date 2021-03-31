182b755d4f58c6d57c2a4f9afe19fe74e68976a3
fcintra4 <github4@faustocintra.com.br>
2021-03-31 14:00:22 +0000

    (31/03) Exercício 6 e exfix03

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -11,15 +11,29 @@ import {PokemonForm, fetchPokemon, PokemonInfoFallback, PokemonDataView} from '.
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
 
-  const [pokemon, setPokemon] = React.useState(null)
+  //const [pokemon, setPokemon] = React.useState(null)
+  //const [error, setError] = React.useState(null)
+  //const [status, setStatus] = React.useState('idle') // Ocioso
+
+  // Reunir todas as variáveis de estado em um único objeto
+  const [state, setState] = React.useState({
+    pokemon: null,
+    error: null,
+    status: 'idle'  // Ocioso
+  })
+  // Usando desestruturação, podemos acessar os atributos do objeto de estado
+  // por meio de variáveis individuais
+  const { pokemon, error, status } = state
 
   React.useEffect(() => {
   
     // Sem pokemonName, não fazemos nada
     if(pokemonName === '') return
 
-    // Limpando os dados do pokemon
-    setPokemon(null)
+    // Limpando os dados do pokemon e do erro
+    //setPokemon(null)
+    //setError(null)
+    setState({pokemon: null, error: null})
 
     // fetchPokemon é uma função assíncrona. Essas funções podem demorar
     // mais ou menos tempo para serem executadas, e, enquanto elas são processadas,
@@ -49,11 +63,18 @@ function PokemonInfo({pokemonName}) {
     async function getPokemon() {
       try {
         // A chamada à função assíncrona é precedida pela palavra-chave await
+        //setStatus('pending')  // Informações pendentes
+        setState({status: 'pending'})
         let data = await fetchPokemon(pokemonName)  // Chamada assíncrona
-        setPokemon(data)
+        //setPokemon(data)
+        //setStatus('resolved') // Requisição remota resolvida com sucesso
+        setState({pokemon: data, status: 'resolved'})
       }
       catch(erro) {
-        alert(erro.message)
+        //alert(erro.message)
+        //setError(erro)
+        //setStatus('rejected') // Requisição remota rejeitada por erro
+        setState({error: erro, status: 'rejected'})
       }
     }
     // Chamada à função
@@ -74,13 +95,23 @@ function PokemonInfo({pokemonName}) {
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  
+  switch(status) {
+    case 'idle':
+      return 'Submit a pokemon'
+    case 'rejected':
+      return (
+        <div role="alert">
+          There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        </div>
+      )
+    case 'pending':
+      return <PokemonInfoFallback name={pokemonName} />
+    // case resolved:
+    default:
+      return <PokemonDataView pokemon={pokemon} />
+  }
 
-  // 💣 remove this
-  if(pokemonName === '') return 'Submit a pokemon'
-  else if(pokemonName !== '' && pokemon === null)
-    return <PokemonInfoFallback name={pokemonName} />
-  else
-    return <PokemonDataView pokemon={pokemon} />
 }
 
 function App() {
